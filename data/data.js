18304a1aa486bb8f7d11614a839110a4c87cbaaa
v3i1ix <v3i1ix@proton.me>
2025-01-02 16:44:30 +1100

    Upgrades people, upgrades!!

---
+++ data/data.js
@@ -0,0 +1,17 @@
+var projects = [
+    /*{
+      title: 'Project 1',
+      image: 'img/project1.jpg',
+      caption: 'Caption for Project 1',
+      description: 'Short description of Project 1',
+      link: 'project1.html'
+    },
+    {
+      title: 'Project 2',
+      image: 'img/project2.jpg',
+      caption: 'Caption for Project 2',
+      description: 'Short description of Project 2',
+      link: 'project2.html'
+    },
+    // Add more projects as needed*/
+  ];
---
+++ script.js
@@ -0,0 +1,107 @@
+// Constants
+const background = document.getElementById('myVideo');
+const headingSection = document.getElementById("welcome-text");
+const maxBlur = 50; // Maximum blur value
+const blurFactorA = 20; // Divisor for scrollY to control blur speed
+const blurFactorB = 40;
+const transpFactorA = 20;
+const opacityThreshold = 10;
+const opacityEnd = 300;
+const audio = document.getElementById("audioPlayer");
+const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+const gainNode = audioContext.createGain();
+const audioSource = audioContext.createMediaElementSource(audio);
+const volumeSlider = document.getElementById('volumeSlider');
+const playPauseBtn = document.getElementById('playPauseBtn');
+const playlist = [
+  { src: './data/audio/deadmau5 - Everything Before.mp3', start: 135, }
+];
+let currentTrackIndex = 0;
+
+// Blur and opacity by scroll
+document.addEventListener('scroll', function () {
+  
+  const scrollY = window.scrollY;
+  const blurValueA = Math.min(scrollY / blurFactorA, maxBlur); // Calculate blur based on scroll
+  const blurValueB = Math.min(scrollY / blurFactorB, maxBlur);
+
+  if (background) {
+    background.style.filter = `blur(${blurValueA}px)`;
+  }
+  if (headingSection) {
+    headingSection.style.filter = `blur(${blurValueB}px)`;
+  }
+
+  // Calculate opacity
+  let opacity = 1;
+  if (scrollY >= opacityThreshold && scrollY < opacityEnd) {
+    opacity = 1 - ((scrollY - opacityThreshold) / (opacityEnd - opacityThreshold));
+  } else if (scrollY >= opacityEnd) {
+    opacity = 0;
+  }
+
+  if (headingSection) {
+    headingSection.style.opacity = opacity;
+  }
+});
+
+audioSource.connect(gainNode);
+gainNode.connect(audioContext.destination);
+
+function fadeIn() {
+  const currentTime = audioContext.currentTime;
+  gainNode.gain.setValueAtTime(0, currentTime);
+  gainNode.gain.linearRampToValueAtTime(1, currentTime + 0.3);
+}
+
+function fadeOut() {
+  const currentTime = audioContext.currentTime;
+  gainNode.gain.setValueAtTime(gainNode.gain.value, currentTime);
+  gainNode.gain.linearRampToValueAtTime(0, currentTime + 0.3);
+}
+
+function playTrack(index) {
+  const track = playlist[index];
+  audio.src = track.src;
+  audio.currentTime = track.start;
+  const trackTitle = track.src.split('/').pop().split('.')[0];
+  document.getElementById('currentTrack').innerText = trackTitle;
+}
+
+playPauseBtn.addEventListener('click', function() {
+  if (audio.paused) {
+    playTrack(currentTrackIndex);
+    audio.play();
+    fadeIn();
+    playPauseBtn.textContent = '◼';
+  } else {
+    fadeOut();
+    setTimeout(() => {
+      audio.pause();
+      playPauseBtn.textContent = '▶';
+    }, 300); // Wait for fade-out to complete (0.3 seconds)
+  }
+});
+
+audio.addEventListener('ended', function() {
+  currentTrackIndex = (currentTrackIndex + 1) % playlist.length;
+  playTrack(currentTrackIndex);
+  audio.play();
+  fadeIn();
+});
+
+volumeSlider.addEventListener('input', function() {
+  gainNode.gain.value = volumeSlider.value / 100;
+});
+
+gainNode.gain.value = volumeSlider.value / 100;
+playTrack(currentTrackIndex);
+
+/*
+next:
+- fix audio player on scroll
+- optimise audio player for mobile
+- add back to top button on scroll
+- see if we can get some transparency animations through the text
+- find an easier way to change variable names all at once
+*/
